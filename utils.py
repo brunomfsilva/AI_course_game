f2c817a9731a2444c62189ad970b95699211bdcc
BrunoSilva <119140240+brunomfsilva@users.noreply.github.com>
2024-02-20 21:45:43 +0000

    add check_piece_to_capture
    to know if are pieces that need to force the capture

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
-
-
-
+def check_piece_to_capture(board, turn):
+    all_pieces = board.all_pieces_black if turn == BLACK else board.all_pieces_white
+    for piece in all_pieces:
+        piece.check_catch(board)  # Updates legal moves considering captures
+        if piece.legal:
+            return True
+    return False
