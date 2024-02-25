0462ee24f5d11bb2956a7d82bbb0e8734cf6a846
Sarara1 <160321883+Sarara1@users.noreply.github.com>
2024-02-25 02:22:34 +0000

    Cores tabuleiro
    Perspetiva utilizador

--- vars.py
+++ vars.py
@@ -1,11 +1,11 @@
-WHITE = (255, 255, 255)
-BLACK = (0, 128, 0)
-GREY1 = (240, 120, 120)
+WHITE = (255, 253, 208)
+BLACK = (200, 42, 42)
+GREY1 = (100, 100, 100)
 GREY2 = (128, 180, 180)
 
 size = 8
 
-width, height = 500, 400
+width, height = 550, 400
 
 #square_size = 50
 square_size = int(min(width, height)/size)
