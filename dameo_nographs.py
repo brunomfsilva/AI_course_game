7915996d4cc47b82252221dd2e274f7d65d8e419
BrunoSilva <119140240+brunomfsilva@users.noreply.github.com>
2024-02-29 21:46:08 +0000

    update dameo_nographs

--- dameo_nographs.py
+++ dameo_nographs.py
@@ -6,38 +6,73 @@ from vars import *
 from player import Player
 import time
 from player import *
+import random
+
+def calculate_time(start_time):
+    return time.time() - start_time
 
 def main():
     """Initiate game"""
     pygame.init()
-    running = True
     gui = GUI()
-    board=Board()
-    board.initialize_pieces()
-    selected_piece = None
-    turn = WHITE
-    winner = None
-    player1 = Player('AI', 'Very easy', WHITE)
-    player2 = Player('AI', 'Hard', BLACK)
+    difficults = ['Very easy', 'Easy', 'Medium', 'Hard']
 
 
-    game_over=False
-    while running:
-            
-        n_plays=0
-        while not game_over and running:
-            
+    sizes = []
+    p1_difs = []
+    p2_difs = []
+    total_plays = []
+    total_time = []
+    p1_medium_time_play = []
+    p2_medium_time_play_catch = []
+    p2_medium_time_play = []
+    p2_medium_time_play_catch = []
+
+
+    for _ in range(2):
+        size = random.randint(5, 7)
+        board = Board()
+        board.size = size
+        board.initialize_pieces()
+        selected_piece = None
+        turn = WHITE
+        winner = None
+        p1_difficult = random.randint(0, 3)
+        p2_difficult = random.randint(0, 3)
+        player1 = Player('AI', difficults[p1_difficult], WHITE)
+        player2 = Player('AI', difficults[p2_difficult], BLACK)
+        
+        print(size, p1_difficult, p2_difficult)
+
+        game_over = False
+        n_plays = 0
+        
+
+        start_time = time.time()
+        p1_times_can_catch = []
+        p2_times_can_catch = []
+        p1_times_no_catch = []
+        p2_times_no_catch = []
+
+        while not game_over:
             for player in (player1, player2):
-                n_plays+=1
-                print(n_plays)
-                
+                catch = False
                 if game_over:
                     break
                 
-                if player.type == 'AI' and player.level == 'Very easy' and turn == player.team:
-                    selected_piece = player.ai_random_move(board, turn)
+                # if player.type == 'AI' and player.level == 'Very easy' and turn == player.team:
+                #     selected_piece = player.ai_random_move(board, turn)
 
-                if player.type == 'AI' and player.level == 'Hard' and turn == player.team:
+                if player.type == 'AI' and player.level != 'Very easy' and turn == player.team:
+                    if player.level == 'Very Easy':
+                        depth= 1
+                    elif player.level == 'Easy':
+                        depth= 2
+                    elif player.level == 'Medium':
+                        depth= 4
+                    elif player.level == 'Hard':
+                        depth= 6
+                    
                     selected_piece, best_move = execute_minimax(board, 5, turn)
                     board.chessboard[selected_piece.row][selected_piece.col] = None
                     selected_piece.move(best_move[0], best_move[1], board)
@@ -64,10 +99,19 @@ def main():
                         turn = WHITE
 
                     #time.sleep(1)
-    
+                
+                n_plays += 1
                 winner = board.check_winner()
 
                 if winner:
+                    end_time = time.time
+                    sizes.append(size)
+                    p1_difs.append(p1_difficult)
+                    p2_difs.append(p2_difficult)
+                    total_plays.append(n_plays)
+                    total_time.append(end_time - start_time)
+
+
                     print(winner + ' Wins!')
                     game_over = True
                     running = False
