1788441fa227d64d7bdb2bccbfc6f179a07e44bc
BrunoSilva <119140240+brunomfsilva@users.noreply.github.com>
2024-03-10 00:25:22 +0000

    size*7 to tie

--- board.py
+++ board.py
@@ -256,7 +256,7 @@ class Board:
             return "Player 2"
         
         #Tie
-        if self.moves_whitout_catching == self.size*5:
+        if self.moves_whitout_catching == self.size*7:
             return "Tie"
         
     def find_available_moves(self, turn):
